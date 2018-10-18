d963e3fc6b123f4a29cb458c52c14f60175ffe67
lanagasparyan <44252006+lanagasparyan@users.noreply.github.com>
2018-10-18 01:30:26 -0400

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -6,11 +6,5 @@ from os import environ
 from BinaryDecimal import app
 
 if __name__ == '__main__':
-    HOST = environ.get('SERVER_HOST', 'localhost')
-    try:
-        PORT = int(environ.get('SERVER_PORT'))
-        app.run(HOST, PORT)
-    except ValueError:
-        #Run in Cpanel
-        app.run()
+    app.run()
     
