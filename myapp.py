86acbe104bd9866992d5b3b70bdade195cf02e30
lanagasparyan <44252006+lanagasparyan@users.noreply.github.com>
2018-10-18 01:39:50 -0400

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -5,6 +5,7 @@ This script runs the BinaryDecimal application using a development server.
 from os import environ
 from BinaryDecimal import app
 
+application = app
 if __name__ == '__main__':
     app.run()
     
