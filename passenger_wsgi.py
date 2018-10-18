596acf22c623bef47e33afe852715b1788b3ad12
lanagasparyan <44252006+lanagasparyan@users.noreply.github.com>
2018-10-18 01:52:26 -0400

    Update passenger_wsgi.py

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,8 +1,1 @@
-import imp
-import os
-import sys
- 
-sys.path.insert(0, os.path.dirname(__file__))
- 
-wsgi = imp.load_source('wsgi', 'myapp.py')
-application = wsgi.application
+from myapp import application
