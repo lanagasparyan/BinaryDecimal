7eba78e3df7cf30c51cf353c818ef0fdbe023883
lanagasparyan <44252006+lanagasparyan@users.noreply.github.com>
2018-10-18 01:29:16 -0400

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -8,8 +8,9 @@ from BinaryDecimal import app
 if __name__ == '__main__':
     HOST = environ.get('SERVER_HOST', 'localhost')
     try:
-        PORT = int(environ.get('SERVER_PORT', '5555'))
+        PORT = int(environ.get('SERVER_PORT'))
+        app.run(HOST, PORT)
     except ValueError:
         #Run in Cpanel
         app.run()
-    app.run(HOST, PORT)
+    
